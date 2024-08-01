d20233d1b439977e1b6db3598c9ac2f7f934487b
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-08-02 00:53:15 +0500

    Time: 3 ms (61.45%) | Memory: 15.4 MB (40.30%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrack(res, "", 0, 0, n);
+        return res;
+    }
+
+    void backtrack(vector<string>& res, string current, int open, int close,
+                   int max) {
+        if (current.length() == max * 2) {
+            res.push_back(current);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(res, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(res, current + ")", open, close + 1, max);
+        }
+    }
+};
