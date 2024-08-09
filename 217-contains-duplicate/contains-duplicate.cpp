ad990615508249c3919d0e72546390ed77d7febd
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-08-09 21:01:39 +0500

    Time: 102 ms (34.22%) | Memory: 73.4 MB (33.62%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        
+        for (auto num : nums) {
+            if (mp.find(num) != mp.end()) {
+                return true; 
+            }
+            mp[num] = 1;
+        }
+        
+        return false;
+    }
+};
