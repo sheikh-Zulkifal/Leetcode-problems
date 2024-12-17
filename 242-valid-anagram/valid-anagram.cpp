98a41543c609a3ce340218584a11019decdefb67
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-12-17 19:57:19 +0500

    Time: 7 ms (33.73%) | Memory: 9.7 MB (50.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,9 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        if (s == t) {
+        if (s == t)
             return true;
-        }
-        return false;
+        return false;   
     }
 };
