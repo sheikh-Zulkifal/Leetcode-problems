93bfd73d42b6cd9a403c5f2bc5926cb384d3c486
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-12-20 15:59:24 +0500

    Time: 0 ms (100.00%) | Memory: 31.2 MB (31.31%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -5,12 +5,13 @@ public:
         int right = nums.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums[mid] == target)
+            if (nums[mid] == target) {
                 return mid;
-            if (nums[mid] > target)
-                right = mid - 1;
-            if (nums[mid] < target)
+            } else if (nums[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return -1;
     }
