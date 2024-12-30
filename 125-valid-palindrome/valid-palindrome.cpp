028b61b76d28f355588beda42667c35687886ac4
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-12-30 16:43:49 +0500

    Time: 0 ms (100.00%) | Memory: 10.1 MB (22.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.size() - 1;
-        while (left < right) {
-            while (left < right && !isalnum(s[left])){
+        int left =0;
+        int right = s.size()-1;
+        while(left < right){
+            while(left < right && !isalnum(s[left])){
                 left++;
-
             }
-            while (left <  right && !isalnum(s[right])){
+            while(left < right && !isalnum(s[right])){
                 right--;
             }
             if(tolower(s[left]) != tolower(s[right])){
@@ -18,5 +17,5 @@ public:
             right--;
         }
         return true;
-    }   
+    }
 };
