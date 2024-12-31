c9e0d1d80a5f8a73a84950d7ae636d70de5e7040
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2024-12-31 22:58:25 +0500

    Time: 43 ms (88.05%) | Memory: 29 MB (57.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,18 +3,14 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-
         if (nums.size() < 3) {
             return res;
         }
-
         for (int i = 0; i < nums.size() - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
             int left = i + 1, right = nums.size() - 1;
-
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum > 0) {
@@ -23,10 +19,12 @@ public:
                     left++;
                 } else {
                     res.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1])
+                    while (left < right && nums[left] == nums[left + 1]) {
                         left++;
-                    while (left < right && nums[right] == nums[right - 1])
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
+                    }
                     left++;
                     right--;
                 }
