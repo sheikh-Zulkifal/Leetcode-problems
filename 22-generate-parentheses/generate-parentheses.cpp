68cfe4a4059e6f7ae55376193f87aa2c81407fa3
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-01-06 20:43:43 +0500

    Time: 3 ms (69.21%) | Memory: 15.6 MB (42.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -5,19 +5,17 @@ public:
         backtrack(res, "", 0, 0, n);
         return res;
     }
-
     void backtrack(vector<string>& res, string current, int open, int close,
                    int max) {
         if (current.length() == max * 2) {
             res.push_back(current);
             return;
         }
-
         if (open < max) {
-            backtrack(res, current + "(", open + 1, close, max);
+            backtrack(res, current + '(', open + 1, close, max);
         }
         if (close < open) {
-            backtrack(res, current + ")", open, close + 1, max);
+            backtrack(res, current + ')', open, close + 1, max);
         }
     }
 };
