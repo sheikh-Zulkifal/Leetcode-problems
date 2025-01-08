18b74de94b41471dad93521bb65c74daed09c664
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-01-08 17:31:21 +0500

    Time: 47 ms (53.91%) | Memory: 85.7 MB (43.21%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> car;
+        vector<pair<int, int>> pair;
         for (int i = 0; i < position.size(); i++) {
-            car.push_back({position[i], speed[i]});
+            pair.push_back({position[i], speed[i]});
         }
-        
-        // Sort cars based on their positions in descending order
-        sort(car.rbegin(), car.rend());
-        
-        vector<double> timeToTarget;
-        for (auto& pair : car) {
-            double time = (target - pair.first) * 1.0 / pair.second;
-            // If the current car's time is less than or equal to the last car in the fleet,
-            // it will join the same fleet.
-            if (!timeToTarget.empty() && time <= timeToTarget.back()) {
-                continue;
+        sort(pair.rbegin(), pair.rend());
+        vector<double> stack;
+        for (auto& p : pair) {
+            stack.push_back((double)(target - p.first) / p.second);
+            if (stack.size() >= 2 && 
+                stack.back() <= stack[stack.size() - 2]) 
+            {
+                stack.pop_back();
             }
-            timeToTarget.push_back(time);
         }
-        
-        // The size of timeToTarget represents the number of fleets
-        return timeToTarget.size();
+        return stack.size();
     }
 };
