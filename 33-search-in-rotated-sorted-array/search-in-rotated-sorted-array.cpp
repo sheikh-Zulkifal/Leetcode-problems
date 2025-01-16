b02a70c2394c9f19fc2d59abb053baa50d8701d7
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-01-16 22:21:21 +0500

    Time: 0 ms (100.00%) | Memory: 15.1 MB (57.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,11 +4,10 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
