497bb50187d81228abb5ec492426c8d068262b07
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-05-29 01:07:13 +0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.74%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,7 +4,11 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] > nums[j]) {
-                    swap(nums[i], nums[j]);
+                    // swap(nums[i], nums[j]);
+                    int temp = 0;
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j]= temp;
                 }
             }
         }
