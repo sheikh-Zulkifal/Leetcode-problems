18d309cfb369d1617d71309c383161081c38164e
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-06-29 00:01:27 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (98.93%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,12 +11,22 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
-        if(list1->val > list2->val){
-            swap(list1,list2);
+        if (!list1) {
+            return list2;
         }
-        list1->next = mergeTwoLists(list1->next,list2);
-        return list1;
+        if (!list2) {
+            return list1;
+        }
+        ListNode* result;
+        if (list1->val < list2->val) {
+            result = list1;
+            result->next = mergeTwoLists(list2, list1->next);
+        }
+        else{
+            result = list2;
+            result->next = mergeTwoLists(list1, list2->next );
+
+        }
+        return result;
     }
 };
