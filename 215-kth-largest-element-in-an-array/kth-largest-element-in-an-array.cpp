dfa154cd7bc426e4636c75ff4dc779af004e0ce3
Zulkifal <126913851+sheikh-Zulkifal@users.noreply.github.com>
2025-07-22 03:42:20 +0500

    Time: 2201 ms (5.02%) | Memory: 59.2 MB (85.01%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,7 +1,39 @@
 class Solution {
 public:
+    // Partition the array so that all elements less than pivot go to the left
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[right]; // Choose pivot as the last element
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivot) { // '<' because we're doing ascending sort
+                swap(nums[i], nums[storeIndex]);
+                storeIndex++;
+            }
+        }
+
+        // Put pivot in its correct position
+        swap(nums[storeIndex], nums[right]);
+        return storeIndex;
+    }
+
+    // Quickselect to find the kth smallest element
+    int quickSelect(vector<int>& nums, int left, int right, int targetIndex) {
+        int pivotIndex = partition(nums, left, right);
+
+        if (pivotIndex == targetIndex) {
+            return nums[pivotIndex];
+        } else if (pivotIndex < targetIndex) {
+            return quickSelect(nums, pivotIndex + 1, right, targetIndex);
+        } else {
+            return quickSelect(nums, left, pivotIndex - 1, targetIndex);
+        }
+    }
+
+    // Main function to find the kth largest using ascending logic
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()-k];
+        int n = nums.size();
+        int targetIndex = n - k; // kth largest is at index n - k in ascending order
+        return quickSelect(nums, 0, n - 1, targetIndex);
     }
 };
